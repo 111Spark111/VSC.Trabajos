156f2e77e86f7143693aa323b055a107b518c7e4
Carlos Rivera <carlosriveraquezada@gmail.com>
2023-04-09 19:37:19 -0400

    se agrega el proyecto

---
+++ Copia.py
@@ -0,0 +1,7 @@
+"Programa para escribir"
+print("Hola mundo")
+nombre = input("pon tu nombre: ")
+name = input("Pon tu apellido: ")
+print ("Hola soy "+ nombre +" " + name)
+edad = input("Cual es tu edad: ")
+print ("hola soy "+ nombre + " "+ name +" y tengo "+ edad +" Años")
---
+++ segundo_trabajo.py
@@ -0,0 +1,22 @@
+#dato de tipo numerico
+estatura = 1.71
+peso = 70
+complejo = 1 +4j
+print("Impresion del numero complejo:",complejo)
+
+#operacion aritmetica basica
+inc = peso/estatura
+print("Mi INC")
+
+#Valores booleanos
+ampolleta = False
+Interruptor = True 
+
+#Con -type- sabemos el tipo de datos que estamos tratando
+print(type(ampolleta))
+
+#Datos tipo array /areglos/ (objetos de tipo coleccion)
+estudiantes = "matias","marco","cristobal"
+num = [1,2,3,4,5,6]
+print(estudiantes)
+print(num)
